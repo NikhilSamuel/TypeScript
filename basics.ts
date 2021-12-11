267f24ab9aee17fe11cd9b95b036f629836c873b
Nikhil <nikhilsamuel1404@gmail.com>
2021-12-11 07:03:23 -0500

    Generics with typescript

--- basics.ts
+++ basics.ts
@@ -45,3 +45,14 @@ function add(a: number, b: number) {
 function printSomething(value: any) {
   console.log(value); // void
 }
+
+//Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
