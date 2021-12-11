b9ba172fbac2cfb8f4501fafc6ce7d6d9b2afc29
Nikhil <nikhilsamuel1404@gmail.com>
2021-12-11 06:23:58 -0500

    Functions and types

--- basics.ts
+++ basics.ts
@@ -35,3 +35,13 @@ let people: Person[]; //adding the brackets creates an array of people object.
 let course: string | number = "TypeScript"; //using union types
 
 course = 1233; //no error
+
+//Function & Types
+
+function add(a: number, b: number) {
+  return a + b; //inference, we dont need to specify return type if there is no need to do it.
+}
+
+function printSomething(value: any) {
+  console.log(value); // void
+}
