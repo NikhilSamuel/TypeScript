f832999accba7f3db2b09ccda1f506c7d90a3a5f
Nikhil <nikhilsamuel1404@gmail.com>
2021-12-11 05:47:25 -0500

    More complex types

--- basics.ts
+++ basics.ts
@@ -2,7 +2,30 @@
 
 let age: number;
 age = 12;
+
 let userName: string;
 userName = "Nikhil";
+
 let isSomething: boolean;
 isSomething = true;
+
+//More Complex types
+
+let hobbies: string[];
+
+hobbies = ["fishing", "frying"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Nikhil",
+  age: 26,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[]; //adding the brackets creates an array of people object.
